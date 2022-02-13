7daa5a0dd42bf3ca4c34bc0976dff33fc37a8740
keensyo <k66mdg55@gmail.com>
2022-02-13 23:10:46 +0900

    change viewDidLoad and inputFormula method

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -11,17 +11,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var formulaLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     
-    @IBAction func inputFormula(_ sender: UIButton) {
-    }
+
     @IBAction func clearCalcuration(_ sender: UIButton) {
     }
     @IBAction func calcurarteAnswer(_ sender: UIButton) {
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ビューがロードされた時点で式と答えのラベルは空にする
+        formulaLabel.text = ""
+        answerLabel.text = ""
     }
-
+    
+    @IBAction func inputFormula(_ sender: UIButton) {
+        // ボタン（Cと＝以外）が押されたら式を表示する
+        guard let formulaText = formulaLabel.text else {
+            return
+        }
+        guard let senderdText = sender.titleLabel?.text else {
+            return
+        }
+        formulaLabel.text = formulaText + senderdText
+    }
 
 }
 
