d945dbf66d56794cf5c3aed3eb3b3f086521723e
keensyo <k66mdg55@gmail.com>
2022-02-13 15:26:42 +0900

    create Outlet and Action

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func inputFormula(_ sender: UIButton) {
+    }
+    @IBAction func clearCalcuration(_ sender: UIButton) {
+    }
+    @IBAction func calcurarteAnswer(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
